0d676f729e1780c23122f2e4b9bcce786d7a6a99
Berkay <sbbuyukkoru@gmail.com>
2022-10-10 20:50:00 +0300

    first

---
+++ components/Portfolio.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+
+const Portfolio = () => {
+  return (
+    <di className="max-w-[1240px] mx-auto py-16 text-center">
+      <h1 className="font-bold text-2xl p-4">Travel Photos</h1>
+      <div className="grid grid-rows-none md:grid-cols-5 p-4 md:gap-4">
+        <div className="w-full h-full col-span-2 md:col-span-3 row-span-2">
+          <Image
+            src="https://images.unsplash.com/photo-1488235742400-36898425c618?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="/"
+            layout="responsive"
+            width="677"
+            height="451"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1569001178320-92c99c3f1a25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="/"
+            layout="responsive"
+            width="215"
+            height="217"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1512822999866-f690681f3952?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="/"
+            layout="responsive"
+            width="215"
+            height="217"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1597226290016-764e1f4767dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="/"
+            layout="responsive"
+            width="215"
+            height="217"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1604173496722-f4cef4c317ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+            alt="/"
+            layout="responsive"
+            width="215"
+            height="217"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80"
+            alt="/"
+            layout="responsive"
+            width="215"
+            height="217"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </di>
+  );
+};
+
+export default Portfolio;
---
+++ pages/work.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Portfolio from "../components/Portfolio";
+
+const work = () => {
+  return (
+    <div>
+      <Hero heading="My Work" message="These are some of my recent works." />
+      <Portfolio />
+    </div>
+  );
+};
+
+export default work;
